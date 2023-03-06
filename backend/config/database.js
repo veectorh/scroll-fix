ece8fa50e3bb69c09b1e2de74cb2020f073fa4ac
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-03-06 12:09:32 -0600

    Apply fix: https://github.com/render-examples/strapi-postgres/pull/14/files\#diff-7c0d5bebd3627d311396723260c0e60b0a929b1e57158b65e683893bbf75abb1

--- backend/config/database.js
+++ backend/config/database.js
@@ -9,5 +9,16 @@ module.exports = ({ env }) => ({
       password: env('DATABASE_PASSWORD'),
       ssl: env.bool('DATABASE_SSL', true),
     },
+    debug: false,
+    acquireConnectionTimeout: 5000,
+    pool: {
+      min: 0,
+      max: 10,
+      createTimeoutMillis: 8000,
+      acquireTimeoutMillis: 8000,
+      idleTimeoutMillis: 8000,
+      reapIntervalMillis: 1000,
+      createRetryIntervalMillis: 100
+    }
   },
 });
