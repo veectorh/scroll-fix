e320523bdf504abf5bc9b84578205d355cec7e27
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-03-09 14:28:32 -0600

    Update API to sort by name

--- frontend/lib/load-speakers.js
+++ frontend/lib/load-speakers.js
@@ -1,6 +1,6 @@
 export async function loadSpeakers() {
   // Fetch data from external API
-  const response = await fetch(`https://speakout-backend-g62z.onrender.com/api/speakers?fields[0]=fullName&fields[1]=tagLine&fields[2]=slug&populate[0]=photo&populate[1]=topics`)
+  const response = await fetch(`https://speakout-backend-g62z.onrender.com/api/speakers?fields[0]=fullName&fields[1]=tagLine&fields[2]=slug&populate[0]=photo&populate[1]=topics&sort[0]=fullName`)
   const data = await response.json()
 
   // Pass data to the page via props
