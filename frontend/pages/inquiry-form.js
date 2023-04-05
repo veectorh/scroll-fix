b938264321f95895a349f9bc88bc251c0597e284
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-05 09:18:37 -0600

    Add max-width to subhead

--- frontend/pages/inquiry-form.js
+++ frontend/pages/inquiry-form.js
@@ -1,6 +1,5 @@
 import styled from "styled-components";
-import Image from 'next/image';
-import Link from 'next/link';
+import Head from 'next/head'
 
 const InquiryFormStyle = styled.section`
   padding: 100px 0 0;
@@ -17,6 +16,7 @@ const InquiryFormStyle = styled.section`
   p {
     color: #2A2A2A;
     margin: 0 0 0;
+    max-width: 950px;
   }
 
   .form {
@@ -37,15 +37,20 @@ const InquiryFormStyle = styled.section`
 `;
 export default function InquiryFormPage() {
   return (
-    <InquiryFormStyle>
-      <h1>SPEAKERS INQUIRY FORM</h1>
-      <p>Please submit the form below to request more information on speakers including fees and availability. For immediate assistance, please call 510-647-9115 or email info@speakoutnow.org. For other inquiries, please use our contact form.<br/><br/>
-      
-      *Required Field.</p>
+    <>
+      <Head>
+        <title>SpeakOut | Inquiry Form</title>
+      </Head>
+      <InquiryFormStyle>
+        <h1>SPEAKERS INQUIRY FORM</h1>
+        <p>Please submit the form below to request more information on speakers including fees and availability. For immediate assistance, please call 510-647-9115 or email info@speakoutnow.org. For other inquiries, please use our contact form.<br/><br/>
+        
+        *Required Field.</p>
 
-      <div className="form">
-        <iframe src="https://speakout.formstack.com/forms/inquiry_form" title="Inquiry Form" width="600" height="1000"></iframe>
-      </div>
-    </InquiryFormStyle>
+        <div className="form">
+          <iframe src="https://speakout.formstack.com/forms/inquiry_form" title="Inquiry Form" width="600" height="1000"></iframe>
+        </div>
+      </InquiryFormStyle>
+    </>
   );
 }
