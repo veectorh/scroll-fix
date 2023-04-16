520c07e23bf6166a7bf0b0d8068efab54993b1cc
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-16 14:40:04 -0600

    Add Google Analytics

--- frontend/pages/_app.js
+++ frontend/pages/_app.js
@@ -1,6 +1,7 @@
 import  NProgress from 'nprogress';
 import Router from 'next/router';
 import Page from '@/components/Page';
+import Script from 'next/script';
 
 Router.events.on('routeChangeStart', () => NProgress.start());
 Router.events.on('routeChangeComplete', () => NProgress.done());
@@ -9,6 +10,19 @@ Router.events.on('routeChangeError', () => NProgress.done());
 export default function App({ Component, pageProps }) {
   return (
     <Page>
+      <Script 
+        strategy="afterInteractive" 
+        src="https://www.googletagmanager.com/gtag/js?id=G-HDHEN3CV9T" 
+      />
+      <Script id="google-analytics" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          function gtag(){dataLayer.push(arguments);}
+          gtag('js', new Date());
+          gtag('config', 'G-HDHEN3CV9T');
+        `}
+      </Script>
+      
       <Component {...pageProps} />
     </Page>    
   )
