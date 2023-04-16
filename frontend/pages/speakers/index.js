3840bf020fc42e1a84d0ba2d6071c98f8101e64d
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-16 17:25:11 -0600

    Update OG

--- frontend/pages/speakers/index.js
+++ frontend/pages/speakers/index.js
@@ -9,11 +9,11 @@ function SpeakersPage({ speakers, topics }) {
             <Head>
                 <title>SpeakOut | Our Speakers</title>
                 <meta property="og:title" content="SpeakOut | Our Speakers"/>
-                <meta property="og:description" content="" />
+                <meta property="og:description" content="Voices Changing Lives" />
                 <meta property="og:type" content="website" />
                 <meta property="og:url" content="https://www.speakoutnow.org/speakers"
                  />
-                <meta property="og:image" content="/images/speakout-logo-dark.png" />
+                <meta property="og:image" content="https://speakout-website.s3.amazonaws.com/speakout_logo_dark_c683312f3e.png" />
             </Head>
             <Speakers speakers={speakers.data} topics={topics} error />
         </div>
