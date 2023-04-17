2e1ff8f397cf4295cb4e3f55462d047bcb878c43
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-17 13:24:57 -0600

    Update gradient

--- frontend/components/styles/HeroStyles.js
+++ frontend/components/styles/HeroStyles.js
@@ -3,17 +3,17 @@ import { device } from '../device';
 const HeroStyles = styled.section`
   text-align: center;
   padding: 278px 0 250px;
-  /* background: #30E8BF;
-  background: -webkit-linear-gradient(to left, #FF8235, #30E8BF);
-  background: linear-gradient(to left, #FF8235, #30E8BF);  */
+  filter: contrast(100%) brightness(100%);
   background-color:hsla(181,100%,35%,1);
-background-image:
-radial-gradient(at 2% 13%, hsla(0,0%,100%,0.1) 0px, transparent 50%),
-radial-gradient(at 98% 6%, hsla(181,78%,18%,1) 0px, transparent 50%),
-radial-gradient(at 39% 40%, hsla(28,92%,64%,0.67) 0px, transparent 50%),
-radial-gradient(at 74% 5%, hsla(181,78%,18%,1) 0px, transparent 50%),
-radial-gradient(at 28% 7%, hsla(28,92%,64%,1) 0px, transparent 50%),
-radial-gradient(at 19% 70%, hsla(181,100%,35%,1) 0px, transparent 50%);
+  background-image:
+  radial-gradient(at 2% 13%, hsla(0,0%,100%,0.1) 0px, transparent 50%),
+  radial-gradient(at 98% 6%, hsla(181,78%,18%,1) 0px, transparent 50%),
+  radial-gradient(at 39% 40%, hsla(28,92%,64%,0.67) 0px, transparent 50%),
+  radial-gradient(at 74% 5%, hsla(181,78%,18%,1) 0px, transparent 50%),
+  radial-gradient(at 28% 7%, hsla(28,92%,64%,1) 0px, transparent 50%),
+  radial-gradient(at 19% 70%, hsla(181,100%,35%,1) 0px, transparent 50%),
+  url("data:image/svg+xml,%3Csvg viewBox='0 0 1000 1000' xmlns='http://www.w3.org/2000/svg'%3E%3Cfilter id='noiseFilter'%3E%3CfeTurbulence type='fractalNoise' baseFrequency='0.57' numOctaves='5' stitchTiles='stitch'/%3E%3C/filter%3E%3Crect width='100%25' height='100%25' filter='url(%23noiseFilter)'/%3E%3C/svg%3E");
+
   h1 {
     font-family: "Oswald", -apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
     font-size: 80px;
