8a93eeafc357d9d8adf18414d36cfc1e7649d591
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-16 18:18:33 -0600

    Add redirect

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -3,7 +3,16 @@ const nextConfig = {
   reactStrictMode: true,
   images: {
     domains: ['speakout-website.s3.amazonaws.com', 'scontent-qro1-1.cdninstagram.com', 'fastly.picsum.photos']
-  }
+  },
+  async redirects() {
+    return [
+      {
+        source: '/find-a-speaker',
+        destination: '/speakers',
+        permanent: true,
+      },
+    ]
+  },
 }
 
 module.exports = nextConfig
