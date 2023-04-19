ad40d525f54f9271b99fb0f053826c7b43f0ca83
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-18 18:31:35 -0600

    Update Video component

--- frontend/components/Videos.js
+++ frontend/components/Videos.js
@@ -2,6 +2,7 @@ import styled from "styled-components";
 import { useState } from "react";
 import VideoModal from "@/components/VideoModal";
 import Image from "next/image";
+import Video from "./Video";
 
 const VideoStyles = styled.section`
   display: flex;
@@ -11,38 +12,12 @@ const VideoStyles = styled.section`
 
   .videos {
     display: flex;
+    flex-direction: row;
     gap: 20px;
-    
-    .video {
-      width: 265px;
-      border-radius: 20px;
-      position: relative;
-      cursor: pointer;
-      
-      p {
-        font-family: 'Oswald';
-        font-style: normal;
-        font-weight: 500;
-        font-size: 20px;
-        line-height: 24px;
-      }
-      .play {
-        position: absolute;
-        top: 42px;
-        left: 100px;
-      }
-      img {
-        border-radius: 20px;
-      }
-    }
   }
 `;
 
-function getYoutubeImage(url) {
-  // https://img.youtube.com/vi/<insert-youtube-video-id-here>/hqdefault.jpg
-  const videoId = new URL(url).searchParams.get("v");
-  return `https://img.youtube.com/vi/${videoId}/mqdefault.jpg`
-}
+
 
 export default function Videos({ videos }) {
   const [showModal, setShowModal] = useState(false);
@@ -61,18 +36,8 @@ export default function Videos({ videos }) {
       <div className="videos">
         {
           videos.map(video => (  
-            <div className="video-container" key={video.id} >
-              <div className="video" onClick={handleButtonClick}>
-                <img src={getYoutubeImage(video.video_url)} width={262} className="thumbnail" />
-                <Image src="/images/orange-play-button.png" width="64" height="64" className="play" alt="Play" />
-                <p>{video.video_name}</p>
-              </div>
-              {video.video_url && showModal && (
-                <VideoModal videoSrc={video.video_url} onClose={handleCloseModal} />
-              )}
-            </div>    
+            <Video key={video.id} video={video} onClose={handleCloseModal} onClick={handleButtonClick} showModal={showModal} />
           ))
-          
         }
       </div>
     </VideoStyles>
