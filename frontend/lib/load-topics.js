c8fd142db482a3bdb80717e6158a49eea3432ff7
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-21 20:58:36 -0600

    Update API to sort topic by name

--- frontend/lib/load-topics.js
+++ frontend/lib/load-topics.js
@@ -1,5 +1,5 @@
 export async function loadTopics() {
-  const response = await fetch(`https://speakout-backend-g62z.onrender.com/api/topics`)
+  const response = await fetch(`https://speakout-backend-g62z.onrender.com/api/topics?sort[0]=name`)
   const data = await response.json()
 
   return data;
