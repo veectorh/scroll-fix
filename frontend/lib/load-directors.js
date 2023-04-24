203fef1d028dee7f280ab7a50d71cdabbd2efb34
Arturo R Cabrera <artrodriguezcabrera@gmail.com>
2023-04-23 21:45:27 -0600

    Update order

--- frontend/lib/load-directors.js
+++ frontend/lib/load-directors.js
@@ -1,5 +1,5 @@
 export async function loadDirectors() {
-  const response = await fetch(`https://speakout-backend-g62z.onrender.com/api/board-members?populate=*`)
+  const response = await fetch(`https://speakout-backend-g62z.onrender.com/api/board-members?populate=*&sort=lastName`)
   const data = await response.json()
 
   return data;
