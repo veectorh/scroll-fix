3ad9ddaf8ff1200739b5cf61cae963a0c6151790
Arpit Gupta <arpitgupta1214@gmail.com>
2023-05-30 23:55:36 +0530

    animation added

---
+++ frontend/components/styles/ScrollStyles.js
@@ -0,0 +1,39 @@
+import styled from "styled-components";
+import { device } from "../device";
+
+const ScrollStyles = styled.section`
+  width: 100%;
+  position: relative;
+  height: calc(60vh + ${({ childrenLen }) => childrenLen * 130}vh);
+
+  .track {
+    width: 100%;
+    height: 101vh;
+    overflow: hidden;
+    position: sticky;
+    top: 0;
+  }
+
+  .placeholders {
+    padding-top: 30vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    z-index: -1;
+    .placeholder {
+      width: 100%;
+      height: 130vh;
+    }
+  }
+
+  @media ${device.tablet} {
+    height: auto;
+    .track {
+      height: auto;
+    }
+  }
+`;
+
+export default ScrollStyles;
