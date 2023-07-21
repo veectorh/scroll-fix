376fdfc988e37e6e14a8d6b65abec3083cc68e0f
veectorh <blackjetv5@gmail.com>
2023-07-22 00:45:42 +0100

    changes to accomodate for scroll speed issues

---
+++ frontend/components/styles/EducationInstStyles.js
@@ -0,0 +1,134 @@
+import styled from "styled-components";
+import { device } from "../device";
+const EducationalInsStyles = styled.section`
+  background: #f2f2f2;
+  text-align: center;
+  padding: 0;
+  p {
+    max-width: 898px;
+    margin: 0 auto 66px;
+  }
+
+  .copy-image {
+    display: flex;
+    align-items: center;
+    width: 100%;
+    /* margin-bottom: -4px; */
+    height: 101vh;
+    position: absolute;
+    top: 0;
+    left: 0;
+    &.visible {
+      z-index: 10;
+      .copy {
+        transform: translateY(0);
+      }
+      .image {
+        transform: translateY(0);
+      }
+    }
+
+    .copy {
+      height: 100%;
+      display: flex;
+      align-items: center;
+      justify-content: center;
+      background: white;
+      .copy-container {
+        max-width: 353px;
+        margin: 0 auto;
+        text-align: left;
+      }
+    }
+    .image {
+      height: 100%;
+
+      img {
+        position: relative !important;
+        object-fit: cover;
+      }
+    }
+
+    #virtual-event {
+      object-position: -90px 0%;
+    }
+    #summer-institute {
+      object-position: -300px 0%;
+    }
+    #original-film {
+      object-position: 0px 0%;
+    }
+  }
+
+  .copy-image:nth-child(even) {
+    flex-direction: row-reverse;
+  }
+
+  .copy-image > * {
+    flex-basis: 100%;
+  }
+
+  @media ${device.tablet} {
+    .copy-image {
+      position: relative;
+      flex-direction: column;
+      height: fit-content;
+      gap: 24px;
+      background: white;
+      .image {
+        transform: none;
+        width: 100%;
+      }
+      .copy {
+        transform: none;
+        .copy-container {
+          max-width: 100%;
+        }
+      }
+    }
+
+    p {
+      width: 100%;
+      max-width: 100%;
+      margin: 0 auto 33px;
+    }
+
+    .copy-image {
+      #virtual-event {
+        object-position: 0px 0%;
+      }
+      #summer-institute {
+        object-position: 0px 0%;
+      }
+      #original-film {
+        /* object-position: -120px 0%; */
+      }
+
+      .image {
+        height: 40vh;
+      }
+    }
+
+    .copy-image:nth-child(even) {
+      flex-direction: column;
+    }
+
+    .copy-image:first-child {
+      h2 {
+        margin-top: 0px;
+      }
+    }
+
+    h2 {
+      //styleName: H1 Mobile;
+
+      font-size: 20px;
+      font-weight: 500;
+      line-height: 28px;
+      letter-spacing: 0.085em;
+      text-align: left;
+    }
+  }
+`;
+
+export default EducationalInsStyles;
--- frontend/pages/index.js
+++ frontend/pages/index.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Head from 'next/head'
 import Link from 'next/link';
 import { loadFeaturedSpeakers } from '@/lib/load-featured-speakers';
@@ -19,10 +19,11 @@ import OurStoreStyles from '../components/styles/OurStoreStyles';
 import ContainerBox from '@/components/styles/ContainerBox';
 import styled from 'styled-components';
 import { device } from '../components/device';
+import EducationInst from "@/components/EducationInst";
 
 const HeroScroll = styled.div`
-  height: 95vh;
-  overflow-y: scroll;
+  /* height: 95vh; */
+  overflow-y: hidden;
 
   @media ${device.tablet} {
     height: unset;
@@ -32,6 +33,8 @@ const HeroScroll = styled.div`
 
 const Home = ({ featuredSpeakers, quotes, products, events }) => {
   const [showModal, setShowModal] = useState(false);
+  const [showRollerSpeed, setShowRollerSpeed] = useState(false);
+  
 
   function handleButtonClick() {
     setShowModal(true);
@@ -41,6 +44,46 @@ const Home = ({ featuredSpeakers, quotes, products, events }) => {
     setShowModal(false);
   }
 
+  var checkScrollSpeed = (function(settings){
+    settings = settings || {};
+  
+    var lastPos, newPos, timer, delta, 
+        delay = settings.delay || 50; // in "ms" (higher means lower fidelity )
+  
+    function clear() {
+      lastPos = null;
+      delta = 0;
+    }
+  
+    clear();
+    
+    return function(){
+      newPos = window.scrollY;
+      if ( lastPos != null ){ // && newPos < maxScroll 
+        delta = newPos -  lastPos;
+      }
+      lastPos = newPos;
+      clearTimeout(timer);
+      timer = setTimeout(clear, delay);
+      return delta;
+    };
+})();
+
+let speed
+let scrollSpeed
+
+
+   // listen to "scroll" event
+   if (typeof window !== 'undefined') {
+      window.onscroll = function(){
+      
+        scrollSpeed = checkScrollSpeed();
+        scrollSpeed > 90 ? setTimeout(() => {setShowRollerSpeed(true)}, 900) :    setTimeout(() => {setShowRollerSpeed(false)}, 900)
+        console.log(showRollerSpeed, "roller speed") 
+      };
+   }
+
+  
   return (
     <>
       <Head>
@@ -78,7 +121,7 @@ const Home = ({ featuredSpeakers, quotes, products, events }) => {
             <p>A digital destination that empowers young people, educators, and professionals through transformative programming featuring our speakers. The goal of our educational programs is to create a more just society.</p>
           </ContainerBox>
         </EducationalIntro>
-        <EducationalInstitute events={events} />
+        {!showRollerSpeed ? <EducationalInstitute events={events} /> : <EducationInst />}
         <QuotesCarousel quotes={quotes.data} />
         <OurStoreStyles>
           <ContainerBox>
